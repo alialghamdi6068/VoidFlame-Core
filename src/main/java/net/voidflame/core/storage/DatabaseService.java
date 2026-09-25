5b0b2cecfe32585618e2135693d81661fc078da7
alialghamdi6068 <alghamdiali6068@gmail.com>
2026-09-25 20:06:50 +0300

    Add core persistent storage API

---
+++ src/main/java/net/voidflame/core/storage/DatabaseService.java
@@ -0,0 +1,16 @@
+package net.voidflame.core.storage;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
+public interface DatabaseService extends AutoCloseable {
+    CompletableFuture<Void> execute(String sql, Object... parameters);
+    CompletableFuture<List<Map<String, Object>>> query(String sql, Object... parameters);
+    CompletableFuture<Integer> update(String sql, Object... parameters);
+    CompletableFuture<Void> transaction(List<Statement> statements);
+    boolean isOpen();
+    String databasePath();
+    record Statement(String sql, Object... parameters) {}
+    @Override void close();
+}
