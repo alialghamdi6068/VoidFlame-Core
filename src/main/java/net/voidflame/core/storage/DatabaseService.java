9b5c213f95152f4d7d55108ae6a9d79b7f0b9bf6
alialghamdi6068 <alghamdiali6068@gmail.com>
2026-09-25 20:09:38 +0300

    Add safe database backup API

--- src/main/java/net/voidflame/core/storage/DatabaseService.java
+++ src/main/java/net/voidflame/core/storage/DatabaseService.java
@@ -1,5 +1,6 @@
 package net.voidflame.core.storage;
 
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
@@ -9,6 +10,7 @@ public interface DatabaseService extends AutoCloseable {
     CompletableFuture<List<Map<String, Object>>> query(String sql, Object... parameters);
     CompletableFuture<Integer> update(String sql, Object... parameters);
     CompletableFuture<Void> transaction(List<Statement> statements);
+    CompletableFuture<Path> backup(Path directory);
     boolean isOpen();
     String databasePath();
     record Statement(String sql, Object... parameters) {}
